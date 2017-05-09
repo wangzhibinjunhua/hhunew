1fb8416878cb2fb8d12dd2b20822127d37283199
wzb <wangzhibin_x@qq.com>
2017-05-09 19:34:06 +0800

    add readdata activity

---
+++ src/com/wzb/hhu/activity/ReadDataActivity.java
@@ -0,0 +1,15 @@
+package com.wzb.hhu.activity;
+
+import android.os.Bundle;
+
+/**
+ * @author wzb<wangzhibin_x@qq.com>
+ * @date May 9, 2017 9:51:19 AM	
+ */
+public class ReadDataActivity extends BaseActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}

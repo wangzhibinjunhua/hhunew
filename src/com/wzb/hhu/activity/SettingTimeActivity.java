28a26dcf7f24b2f13551af6f8325e8570a09b544
wzb <wangzhibin_x@qq.com>
2017-05-24 15:20:38 +0800

    add date time picker

--- src/com/wzb/hhu/activity/SettingTimeActivity.java
+++ src/com/wzb/hhu/activity/SettingTimeActivity.java
@@ -1,4 +1,6 @@
 package com.wzb.hhu.activity;
+import java.util.Calendar;
+
 import com.wzb.hhu.R;
 import com.wzb.hhu.util.ResTools;
 
@@ -6,14 +8,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import cn.qqtheme.framework.picker.DatePicker;
+import cn.qqtheme.framework.picker.TimePicker;
+import cn.qqtheme.framework.util.ConvertUtils;
 
-public class SettingTimeActivity extends BaseActivity{
+public class SettingTimeActivity extends BaseActivity implements OnClickListener{
 	
 	private ImageView backView;
 	private TextView titleView;
+	
+	private Button readBtn,writeBtn,returnBtn;
+	private CheckBox sysClockCb;
+	private EditText sysDate,sysTime;
+	private ImageView dateSet,timeSet;
+	private TextView meterDate,meterTime;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,32 @@ public class SettingTimeActivity extends BaseActivity{
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_settingtime);
 		initTitleView();
+		
+		initView();
+	}
+	
+	private void initView(){
+		sysClockCb=(CheckBox)findViewById(R.id.sys_clock_cb);
+		sysClockCb.setChecked(false);
+		
+		sysDate=(EditText)findViewById(R.id.sys_clock_date_value);
+		sysTime=(EditText)findViewById(R.id.sys_clock_time_value);
+		
+		dateSet=(ImageView)findViewById(R.id.sys_clock_date_value_set);
+		dateSet.setOnClickListener(this);
+		
+		timeSet=(ImageView)findViewById(R.id.sys_clock_time_value_set);
+		timeSet.setOnClickListener(this);
+		
+		meterDate=(TextView)findViewById(R.id.meter_clock_date_value);
+		meterTime=(TextView)findViewById(R.id.meter_clock_time_value);
+		
+		readBtn=(Button)findViewById(R.id.time_read_btn);
+		readBtn.setOnClickListener(this);
+		writeBtn=(Button)findViewById(R.id.time_write_btn);
+		writeBtn.setOnClickListener(this);
+		returnBtn=(Button)findViewById(R.id.time_back_btn);
+		returnBtn.setOnClickListener(this);
 	}
 	
 	private void initTitleView() {
@@ -37,5 +76,81 @@ public class SettingTimeActivity extends BaseActivity{
 			}
 		});
 	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+		case R.id.time_back_btn:
+			finish();
+			break;
+		case R.id.sys_clock_date_value_set:
+			onYearMonthDayPicker();
+			break;
+		case R.id.sys_clock_time_value_set:
+			onTimePicker();
+			break;
+		default:
+			break;
+		}
+	}
+	
+	private void updateSysDateValue(String year, String month, String day){
+		sysDate.setText(year+"-"+month+"-"+day);
+	}
+
+	private void updateSysTimeValue(String hour,String minute){
+		sysTime.setText(hour+":"+minute+":"+"30");
+	}
+	
+	public void onYearMonthDayPicker() {
+        final DatePicker picker = new DatePicker(this);
+        picker.setCanceledOnTouchOutside(true);
+        picker.setUseWeight(true);
+        picker.setTopPadding(ConvertUtils.toPx(this, 20));
+        picker.setRangeEnd(2111, 1, 11);
+        picker.setRangeStart(2016, 1, 1);
+        picker.setSelectedItem(2017, 5, 24);
+        picker.setOnDatePickListener(new DatePicker.OnYearMonthDayPickListener() {
+            @Override
+            public void onDatePicked(String year, String month, String day) {
+            	updateSysDateValue(year, month, day);
+            }
+        });
+        picker.setOnWheelListener(new DatePicker.OnWheelListener() {
+            @Override
+            public void onYearWheeled(int index, String year) {
+                picker.setTitleText(year + "-" + picker.getSelectedMonth() + "-" + picker.getSelectedDay());
+            }
+
+            @Override
+            public void onMonthWheeled(int index, String month) {
+                picker.setTitleText(picker.getSelectedYear() + "-" + month + "-" + picker.getSelectedDay());
+            }
+
+            @Override
+            public void onDayWheeled(int index, String day) {
+                picker.setTitleText(picker.getSelectedYear() + "-" + picker.getSelectedMonth() + "-" + day);
+            }
+        });
+        picker.show();
+    }
+	
+	 public void onTimePicker() {
+	        TimePicker picker = new TimePicker(this, TimePicker.HOUR_24);
+	        picker.setRangeStart(0, 0);//00:00
+	        picker.setRangeEnd(23, 59);//23:59
+	        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+	        int currentMinute = Calendar.getInstance().get(Calendar.MINUTE);
+	        picker.setSelectedItem(currentHour, currentMinute);
+	        picker.setTopLineVisible(false);
+	        picker.setOnTimePickListener(new TimePicker.OnTimePickListener() {
+	            @Override
+	            public void onTimePicked(String hour, String minute) {
+	            	updateSysTimeValue(hour,minute);
+	            }
+	        });
+	        picker.show();
+	    }
 
 }

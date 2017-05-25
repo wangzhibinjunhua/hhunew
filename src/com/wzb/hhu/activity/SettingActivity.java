c2ce629f7d579994e0a94ed0b2d77e55a13def26
wzb <wangzhibin_x@foxmail.com>
2017-05-26 07:46:06 +0800

    update setting activity

--- src/com/wzb/hhu/activity/SettingActivity.java
+++ src/com/wzb/hhu/activity/SettingActivity.java
@@ -1,5 +1,40 @@
 package com.wzb.hhu.activity;
 
-public class SettingActivity extends BaseActivity{
+import com.wzb.hhu.R;
+import com.wzb.hhu.util.ResTools;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SettingActivity extends BaseActivity{
+	
+	private ImageView backView;
+	private TextView titleView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_setting);
+		initTitleView();
+	}
+	
+	private void initTitleView() {
+		backView = (ImageView) findViewById(R.id.title_back);
+		titleView = (TextView) findViewById(R.id.title_text);
+		titleView.setText(ResTools.getResString(SettingActivity.this, R.string.meter_setting));
+		backView.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+	}
 }

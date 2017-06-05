8a9d9727137c2ac610e2793f36c52ef939cf91ce
wzb <wangzhibin_x@qq.com>
2017-06-05 15:27:45 +0800

    update user db

--- src/com/wzb/hhu/util/DbUtil.java
+++ src/com/wzb/hhu/util/DbUtil.java
@@ -1,22 +1,35 @@
 package com.wzb.hhu.util;
 
 import com.wzb.hhu.bean.UserBean;
+import com.wzb.hhu.interf.WApplication;
+
+import android.content.ContentValues;
 
 public class DbUtil {
 	
 	
-	public static void addUser(){
-		
+	public static void addUser(String account,String password,String name,String permission ){
+		ContentValues values=new ContentValues();
+		values.put("account", account);
+		values.put("password", password);
+		values.put("name", name);
+		values.put("permission", permission);
+		WApplication.db.insert("user", null, values);
+		WApplication.db.close();
 		
 	}
 	
-	public static void updateUser(){
-		
-		
+	public static void updateUser(String account,String password,String name,String permission){
+		ContentValues values=new ContentValues();
+		values.put("account", account);
+		values.put("password", password);
+		values.put("name", name);
+		values.put("permission", permission);
+		WApplication.db.update("user", values, "account=?", new String[]{account});
 	}
 	
-	public static void deleteUser(){
-		
+	public static void deleteUser(String account){
+		WApplication.db.delete("user", "account=?", new String[]{account});
 		
 	}
 	

1d7917f16d54dc971a8e366f087184bbb1b1ca1a
wzb <wangzhibin_x@qq.com>
2017-06-16 18:44:32 +0800

    update iec cmd

--- src/com/wzb/hhu/btcore/IECCommand.java
+++ src/com/wzb/hhu/btcore/IECCommand.java
@@ -5,5 +5,14 @@ package com.wzb.hhu.btcore;
  * @date Jun 11, 2017 11:28:51 PM
  */
 public class IECCommand {
+	
+	private static final String READ_CMD_HEAD="01523102";
+	private static final String READ_CMD_END="282903";
+	
+	
+	//合相总有功电量
+	public static void cmd2(){
+		
+	} 
 
 }
